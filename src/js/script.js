d09e9918b7a60fd564d96484d6d3530e4b71ea46
wiktorjs <wiktorj.sienkiewicz@gmail.com>
2023-04-05 22:19:22 +0200

    Adjust sections, include mixins for repetetive code and add initial header animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,24 @@
+// import "./animated/whyImage";
+import "./animated/headerImage"
+
 const headerImage = document.querySelector('.header__title--svg-image');
-console.log(headerImage);
 
 headerImage.style.height = `${headerImage.getBoundingClientRect().width / 1.52}px`
 window.addEventListener("resize", e => {
     headerImage.style.height = `${headerImage.getBoundingClientRect().width / 1.52}px`
 })
+
+const nav = document.querySelector('nav');
+const navBtn = document.querySelector('.nav-menu__button');
+navBtn.addEventListener('click', (e) => {
+    const btnParent = e.target.closest('.nav-menu__button');
+    const btnChildren = btnParent.querySelectorAll('*');
+
+    btnChildren.forEach(btnChild => {
+        btnChild.classList.toggle('active');
+    });
+    btnParent.classList.toggle('box--active');
+    nav.classList.toggle('nav--visible');
+})
+
+
