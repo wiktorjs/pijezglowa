dbc7bd90dc1f78b2b3fd13ce979b28370c296225
wiktorjs <wiktorj.sienkiewicz@gmail.com>
2023-04-08 13:50:38 +0200

    Further sections improvements

--- src/js/animationView.js
+++ src/js/animationView.js
@@ -1,8 +1,7 @@
 class AnimationView {
-  constructor(section, animation) {
+  constructor(section) {
     this._section = document.getElementById(`${section}`);
     this._object = this._section.querySelector('object');
-    this._animation = animation;
 
     this._object.addEventListener('load', this.observeSection.bind(this));
   }
@@ -12,28 +11,53 @@ class AnimationView {
 
     const svgElement = this._object.contentDocument.querySelector('svg');
 
-    console.log(entry.isIntersecting)
     if (!entry.isIntersecting) {
-        // Prevent animation from playing if section is not intersected
-        svgElement.classList.add('stop-animation');
-        return
-    };
+      // Prevent animation from playing if section is not intersected
+      svgElement.classList.add('stop-animation');
+      return;
+    }
 
     svgElement.classList.remove('stop-animation');
 
+    this.animateContent();
   }
 
-  sectionObserver = new IntersectionObserver(this.playAnimation.bind(this), {
+  #sectionObserver = new IntersectionObserver(this.playAnimation.bind(this), {
     // % of section visibility to trigger playAnimation method
     threshold: 0.6,
   });
 
+  #headerObserver = new IntersectionObserver(this.playAnimation.bind(this), {
+    rootMargin: '10%',
+  });
+
+  animateContent() {
+    const contentBox = this._section
+      .querySelector('.content-box')
+      .querySelector('.content-box__text-box');
+    if (!contentBox) return;
+
+    const sectionTitle = contentBox.querySelector('.section-title');
+    const sectionText = contentBox.querySelector('.section-text');
+
+    sectionTitle.classList.add('section-title--visible');
+    sectionText.classList.add('section-text--visible');
+  }
+
   observeSection() {
-    this.sectionObserver.observe(this._section);
+    this._section.classList.contains('header')
+      ? this.#headerObserver.observe(this._section)
+      : this.#sectionObserver.observe(this._section);
   }
 }
 
 // ! === SECTIONS ===
+
+class Header extends AnimationView {
+  constructor() {
+    super('header');
+  }
+}
 
 class About extends AnimationView {
   constructor() {
@@ -47,7 +71,22 @@ class Goals extends AnimationView {
   }
 }
 
+class Why extends AnimationView {
+  constructor() {
+    super('why');
+  }
+}
+
+class Contact extends AnimationView {
+  constructor() {
+    super('contact');
+  }
+}
+
+const HeaderView = new Header();
 const AboutSection = new About();
 const GoalsSection = new Goals();
+const WhySection = new Why();
+const ContactSection = new Contact();
 
-export { AboutSection, GoalsSection };
+export { HeaderView, AboutSection, GoalsSection, WhySection, ContactSection };
